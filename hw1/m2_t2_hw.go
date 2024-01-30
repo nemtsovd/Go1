de0477c1e9492307cd315fe03653870fdcd65444
DN <free11@list.ru>
2024-01-30 22:44:16 +0300

    task 4

--- hw1/m2_t2_hw.go
+++ hw1/m2_t2_hw.go
@@ -4,3 +4,30 @@
 Вход: 1221  Выход: 1221 - палиндром
 Вход: 1234  Выход: 1234 - не палиндром
 */
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+	var src int
+	var rev int
+
+	fmt.Print("Введите число: ")
+	fmt.Scanf("%d", &src)
+
+	num := src
+    for num > 0 {
+		tmp := num % 10
+		rev = rev * 10 + tmp
+		num /= 10
+	}
+
+	if src == rev {
+		fmt.Println("Палиндром")
+	} else {
+		fmt.Println("Не палиндром")
+	}
+}
