26bf69ee1354670cfd9e520350e0e3288f93f349
DN <free11@list.ru>
2024-01-30 22:24:33 +0300

    task 3

--- hw1/m2_t1_hw.go
+++ hw1/m2_t1_hw.go
@@ -4,3 +4,39 @@
 Вход: 1, 9, 2
 Выход: 1, 2, 9
 */
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+	var num1 int
+	var num2 int
+	var num3 int
+	var tmp int
+	
+	fmt.Println("Введите три числа: ")
+	fmt.Scanf("%d", &num1)
+	fmt.Scanf("%d", &num2)
+	fmt.Scanf("%d", &num3)
+
+    if num1 > num2 {
+		tmp = num1
+		num1 = num2
+		num2 = tmp
+	}
+  	if num2 > num3 {
+		tmp = num2
+		num2 = num3
+		num3 = tmp
+  	}
+  	if num1 > num2 {
+		tmp = num1
+		num1 = num2
+		num2 = tmp
+  	}
+ 
+  	fmt.Printf("%d, %d, %d\n", num1, num2, num3)
+}
